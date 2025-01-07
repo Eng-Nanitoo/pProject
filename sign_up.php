5b49d5accbb349dd5e72102d1ded5e2ecce57bb4
eng-Nanitoo <i20378.etu@iscae.mr>
2025-01-07 11:13:43 -1200

    'Update files'

--- sign_up.php
+++ sign_up.php
@@ -1,3 +1,36 @@
+<?php
+    include 'connect.php';
+    $nameError = $emailError = $passwordError = "";
+
+    if(isset($_POST['submit'])){
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $email = $_POST['email'];
+        // if(strlen($password) < 8){
+        //     $passwordError = "Password must contain 8 characters";
+        // }
+        // if(){
+            $sql = "SELECT * FROM user WHERE username='$username' or email='$email'";
+            $result = $conn->query($sql);
+            if($result->num_rows == 0){
+                $sql = "INSERT INTO user(username,email,password) values('$username','$email','$password')";
+                $conn->query($sql);
+                echo "Row effected";
+            }
+            else{
+                $sql1 = "SELECT username FROM user WHERE username='$username'";
+                $sql2 = "SELECT email FROM user WHERE email='$email'";
+                $result1 = $conn->query($sql1);
+                $result2 = $conn->query($sql2);
+                if($result1->num_rows > 0){
+                    $nameError = "username exist";
+                }
+                if($result2->num_rows > 0){
+                    $emailError = "email exist";
+                }
+            }
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -17,13 +50,16 @@
             <h3>Personnal Informations</h3>
             <p>Entrer Your Information In The Field Bllow</p>
             <div class="infos">
-                <form action="sign_in.php" method="post">
-                    <label for="username">Username</label> <input type="text" name="username" required>
+                <form method="post">
+                    <label for="username">Username</label><input type="text" name="username" required>
+                    <span><?php echo $nameError?></span>
                     <label for="email">Email</label> <input type="email" name="email" required>
+                    <span><?php echo $emailError?></span>
                     <label for="Password">Password</label><input type="password"  name="password" required>
+                    <span><?php echo $passwordError?></span>
                     <div class="button-group">
                         <button type="reset" class="cancel-btn">Cancel</button>
-                        <button type="submit" class="login-btn">Login</button>
+                        <button type="submit" name="submit" class="login-btn">Login</button>
                     </div>
                     <p class="p-sign">You already have an account? <a href="sing_in.html">Se connecter</a></p>
                 </form>
